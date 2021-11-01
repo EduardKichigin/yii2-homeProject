5a517b803002e170f91141498a0eb3aec955401b
Eduard <Eduard.kgn@gmail.com>
2021-11-01 21:56:44 +0200

    добавил в админку статистику сайта и модель order c помощью gii

--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -10,26 +10,5 @@ use yii\web\Controller;
 class AppAdminController extends Controller
 {
 
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::class,
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => ['login'],
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-        ];
-    }
-
-
 
 }
